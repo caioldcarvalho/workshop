3cc5c1c7936ec4e3595c0f8d6658cdea094fd7a3
Caio Carvalho <caioldcarvalho@protonmail.com>
2023-05-15 18:01:54 -0300

    refactor: add semantic comments to methods

--- src/ImageFile.php
+++ src/ImageFile.php
@@ -14,8 +14,7 @@ class ImageFile
      */
     function getMiniatures()
     {
-        // Quantity detected automatically, just create as many sets of width/height
-        // as you needed
+        // Quantity detected automatically, just create as many sets of width/height as you need.
         $miniatures = [
             [
                 'width' => 362,
@@ -51,22 +50,46 @@ class ImageFile
         return $str;
     }
 
+    /**
+     * Creates the miniatures for an image file, based on a given name and on specified dimensions.
+     * Uses the `getMiniatures()` method to define how many miniatures will be generated and create them based on their dimensions.
+     * Please refer to `getMiniatures` and edit it to define all needed miniatures.
+     * Always outputs jpg files.
+     */
     function makeMiniatures(string $file, string $miniDir)
     {
+        // Use native PHP function to get name of the file without its extension.
         $path = pathinfo($file);
         $fileName = $path['filename'];
 
+        // Store miniatures' dimensions in array
         $minis = $this->getMiniatures();
 
         foreach ($minis as $mini) {
+
+            /**
+             * Resize the image so that the largest side fits within the limit; the smaller
+             * side will be scaled to maintain the original aspect ratio
+             * example from https://image.intervention.io/v2/api/resize
+             */ 
             $miniature = Image::make($file)->resize($mini['width'], $mini['height'], function ($constraint) {
                 $constraint->aspectRatio();
                 $constraint->upsize();
             });
+
+            /**
+             * Save miniature with a specific filename format. E.g.: "My Sweet Puppy.png" with a 320x280 
+             * mini becomes "my-sweet-puppy-320x280.jpg"
+             */ 
             $miniature->save($miniDir . $fileName . "-" . $mini['width'] . "x" . $mini['height'] . ".jpg", 80);
         }
     }
 
+    /**
+     * Creates name, output directory and output file for specific image.
+     * Adds watermark to image and creates miniatures.
+     * @param array
+     */
     function processSingleFile(array $file)
     {
         $name = $file['name'];
@@ -85,6 +108,12 @@ class ImageFile
         $this->makeMiniatures($fullDir . $name, $miniDir);
     }
 
+    /**
+     * Loops through all files and process each one separately.
+     * Call it via
+     * `$yourObject->processAllFiles($_FILES["your_file_form_element"]);`
+     * @param array
+     */
     function processAllFiles(array $files): void
     {
         for ($i = 0; $i < count($files["name"]); $i++) {
