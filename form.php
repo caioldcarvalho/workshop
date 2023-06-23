610a36a6580603a0521000a2be0a6ff2cde94c43
Caio Carvalho <caioldcarvalho@protonmail.com>
2023-06-23 11:36:45 -0300

    refactor: give name to form

--- form.php
+++ form.php
@@ -1,6 +1,7 @@
 <!-- form -->
 
 <form action="index.php" method="post" enctype="multipart/form-data">
+    <h2>Create files and minis</h2>
     <label for="file">Insert file</label>
     <input type="file" name="files[]" id="file" multiple accept=".jpg, .jpeg, .png, .webp">
     <button type="submit">Send</button>
